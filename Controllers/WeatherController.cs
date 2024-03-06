1288ad83890dd6453c13c8094755d29d888296f9
Robert Greenawalt <robert.greenawalt29@gmail.com>
2024-03-05 20:01:24 -0500

    Initial commit

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController: ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetWeather()
+        {
+            return Ok("hello world");
+        }
+    }
+}
